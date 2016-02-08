46f6565aa145d0513d9b1ffbd6a7b127c6b245e5
Iñaki <inaki.diaz@ikasle.egibide.org>
2016-02-08 19:03:05 +0100

    Delete prueba.py

--- prueba.py
+++
@@ -1,6 +0,0 @@
-import serial
-
-ser = serial.Serial('/dev/tty.Bluetooth-Incoming-Port')  # open serial port
-print(ser.name)         # check which port was really used
-ser.write(b'hello')     # write a string
-ser.close()             # close port
